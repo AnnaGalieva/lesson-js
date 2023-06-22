f43e381bdb6cbe59f9b84de93cb0e0e67245e593
Anna <anutasash@mail.ru>
2023-06-22 22:16:01 +0300

    task

---
+++ dz_seminar11/featured.js
@@ -0,0 +1,57 @@
+const featured = `
+[{
+"id":"1",
+"images":"http://127.0.0.1:5500/dz_seminar11/img/product1.svg",
+"productTitle":"ELLERY X M'O CAPSULE",  
+"productTexting":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", 
+"currency":"$",
+"price":"52",
+"hoverImage":"http://127.0.0.1:5500/dz_seminar11/img/product-hover.svg"
+},
+{
+"id":"2",
+"images":"http://127.0.0.1:5500/dz_seminar11/img/product2.svg",
+"productTitle":"ELLERY X M'O CAPSULE",  
+"productTexting":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+"currency":"$", 
+"price":"52",
+"hoverImage":"http://127.0.0.1:5500/dz_seminar11/img/product-hover.svg"},
+{
+"id":"3",
+"images":"http://127.0.0.1:5500/dz_seminar11/img/product3.svg",
+"productTitle":"ELLERY X M'O CAPSULE",  
+"productTexting":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", 
+"currency":"$",
+"price":"52",
+"hoverImage":"http://127.0.0.1:5500/dz_seminar11/img/product-hover.svg"
+
+},
+{
+"id":"4",
+"images":"http://127.0.0.1:5500/dz_seminar11/img/product4.svg",
+"productTitle":"ELLERY X M'O CAPSULE",  
+"productTexting":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", 
+"currency":"$",
+"price":"52",
+"hoverImage":"http://127.0.0.1:5500/dz_seminar11/img/product-hover.svg"
+
+},
+{
+"id":"5",
+"images":"http://127.0.0.1:5500/dz_seminar11/img/product5.svg",
+"productTitle":"ELLERY X M'O CAPSULE",  
+"productTexting":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", 
+"currency":"$",
+"price":"52",
+"hoverImage":"http://127.0.0.1:5500/dz_seminar11/img/product-hover.svg"
+},
+{
+"id":"6",
+"images":"http://127.0.0.1:5500/dz_seminar11/img/product6.svg",
+"productTitle":"ELLERY X M'O CAPSULE",  
+"productTexting":"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.", 
+"currency":"$",
+"price":"52",
+"hoverImage":"http://127.0.0.1:5500/dz_seminar11/img/product-hover.svg"
+}
+]`;
---
+++ seminar11/data.js
@@ -0,0 +1,24 @@
+const dataProducts = `
+[{
+"id":1,
+"images":"http://127.0.0.1:5500/seminar11/img/product1.svg.svg",
+"product_heading":"MANGO PEOPLE T-SHIRT",
+"currency":"$",
+"price":"300",
+"color":"Red",
+"size":"Xl",
+"Quantity":"Quantity:",
+"closeModalBtn":"http://127.0.0.1:5500/seminar11/img/close.svg.svg"
+},
+{
+"id":2,
+"images":"http://127.0.0.1:5500/seminar11/img/product2.svg.svg",
+"product_heading":"ELLERY X M'O CAPSULE",
+"currency":"$",
+"price":"52",
+"color":"Black",
+"size":"Xl",
+"Quantity":"Quantity:",
+"closeModalBtn":"http://127.0.0.1:5500/seminar11/img/close.svg.svg"
+    }
+]`;
